ccbdc7860bfd93c78be785562be704f0f8ba196d
ZanSky10 <116982563+ZanSky10@users.noreply.github.com>
2022-10-30 22:30:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 global.DeveloperMode = 'false' //true atau false, true berati iya false berati tidak Moga paham
-global.owner = ['6288270863279'] // masuk in no lu sebagai owner
-global.mods = ['6288270863279'] // masuk in no moderator ny
+global.owner = ['62895330083204'] // masuk in no lu sebagai owner
+global.mods = ['6285740647031'] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -30,15 +30,15 @@ global.APIKeys = { // Apikey lu di sini
 }
 
 // Sticker Watermark
-global.packname = 'DanzBot'
-global.author = 'DanzBot'
+global.packname = 'ZanSky BOT'
+global.author = 'ZanSky'
 
 global.multiplier = 36 // ini butuh berapa xp buat levelup 
 
 global.wait = '*WAIT SABAR🥴*'
 global.error = '*ERROR BANH MAAP🗿*'
-global.botwm = 'DanzBot' // watermark bot
-global.oname = 'Danz Owner' // nama owner nya
+global.botwm = 'ZanSky BOT' // watermark bot
+global.oname = 'Dika Store' // nama owner nya
 global.thum = 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 
 let chalk = require('chalk')
